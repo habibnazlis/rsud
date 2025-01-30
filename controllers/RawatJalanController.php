3ebdfdf8580257dc529a8bd9a6aa14c8f73b2346
habibnazlis <habibnazlis23@gmail,com>
2025-01-30 10:29:46 +0700

    printout perbaikan kedua

--- controllers/RawatJalanController.php
+++ controllers/RawatJalanController.php
@@ -3,7 +3,7 @@
 namespace app\controllers;
 
 use Yii;
-use kartik\mpdf\Pdf;
+use Mpdf\Mpdf;
 use app\models\RawatJalan;
 use app\models\RawatJalanSearch;
 use yii\web\Controller;
@@ -253,34 +253,10 @@ class RawatJalanController extends Controller
 
         ]);
     
-        // setup kartik\mpdf\Pdf component
-        $pdf = new Pdf([
-            // set to use core fonts only
-            'mode' => Pdf::MODE_CORE, 
-            // A4 paper format
-            'format' => Pdf::FORMAT_A4, 
-            // portrait orientation
-            'orientation' => Pdf::ORIENT_PORTRAIT, 
-            // stream to browser inline
-            'destination' => Pdf::DEST_BROWSER, 
-            // your html content input
-            'content' => $content,  
-            // format content from your own css file if needed or use the
-            // enhanced bootstrap css built by Krajee for mPDF formatting 
-            'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
-            // any css to be embedded if required
-            'cssInline' => '.kv-heading-1{font-size:18px}', 
-            // set mPDF properties on the fly
-            'options' => ['title' => 'Rawat Jalan Pasien'],
-            // call mPDF methods on the fly
-            'methods' => [ 
-                'SetHeader'=>['Krajee Report Header'], 
-                'SetFooter'=>['{PAGENO}'],
-            ]
-        ]);
-        
-        // return the pdf output as per the destination setting
-        return $pdf->render(); 
+        // setup \mpdf\Pdf component
+        $pdf = new Mpdf();
+        $pdf->WriteHTML($content); 
+        $pdf->Output('Laporan.pdf', \Mpdf\Output\Destination::INLINE);
     }	
 
 }
--- views/rawat-jalan/print.php
+++ views/rawat-jalan/print.php
@@ -24,26 +24,51 @@ use yii\helpers\Html;
         h3 {
             text-align: center;
         }
+        #ttd{
+            text-align: center;
+        }
+        #biodata{
+            position: relative;
+            top: 40px;
+            right:0px;
+            width: 300px;
+        }
     </style>
 </head>
 <body>
-    <h3>Lembar Formulir Rawat Jalan</h3>
-    <p><strong>Nama Lengkap:</strong> <?= Html::encode($model->pasien->nama_pasien) ?></p>
-    <p><strong>Tanggal Lahir:</strong> <?= Html::encode($model->pasien->tgl_lahir) ?></p>
-    <p><strong>No. Rekam Medis:</strong> <?= Html::encode($model->pasien->no_rekam_medis) ?></p>
+    <div id="biodata">
+        <p>Nama Lengkap: <strong><?= Html::encode($model->pasien->nama_pasien) ?></strong></p>
+        <p>Tanggal Lahir: <strong><?= Html::encode($model->pasien->tgl_lahir) ?></strong></p>
+        <p>No. Rekam Medis: <strong><?= Html::encode($model->pasien->no_rekam_medis) ?></strong></p>
+    </div> 
+    <h3>Lembar Formulir Rawat Jalan <br>
+        Layanan Kedokteran Fisik dan Rehabilitas
+    </h3>
+    
     <p><strong>Tanggal Pelayanan:</strong> <?= Html::encode($model->tgl_pelayanan) ?></p>
     <p><strong>Anamnesis:</strong> <?= Html::encode($model->anamnesis) ?></p>
-    <p><strong>Pemeriksaan ICD-10:</strong> <?= Html::encode($model->diagnosis_medis) ?></p>
-    <p><strong>Diagnosis Fungsi:</strong> <?= Html::encode($model->diagnosis_fungsi) ?></p>
+    <p><strong>Pemeriksaan Tindakan Uji Fungsi:</strong> <?= Html::encode($model->tindakan_uji_fungsi) ?></p>
+    <p><strong>Diagnosis Medis (ICD-10):</strong> <?= Html::encode($model->diagnosis_medis) ?></p>
+    <p><strong>Diagnosis Fungsi (ICD-10):</strong> <?= Html::encode($model->diagnosis_fungsi) ?></p>
+    <p><strong>Pemeriksaan Penunjang:</strong> <?= Html::encode($model->pemeriksaan_penunjang) ?></p>
+    <p><strong>Tata Laksana KFR (ICD-9 CM):</strong> <?= Html::encode($model->tata_laksana_kfr) ?></p>
+    <p><strong>Anjuran:</strong> <?= Html::encode($model->anjuran) ?></p>
+    <p><strong>Evaluasi:</strong> <?= Html::encode($model->evaluasi) ?></p>
+    <p><strong>Suspek Penyakit Akibat Kerja:</strong> <?= Html::encode($model->suspek_akibat_kerja) ?></p>
+    <p><strong>Perminataan Terapi:</strong> <?= Html::encode($model->permintaan_terapi) ?></p>
+
     <h4>Program</h4>
     <table>
         <thead>
             <tr>
-                <th>Program</th>
-                <th>Tanggal</th>
-                <th>Tanda Tangan Pasien</th>
-                <th>Tanda Tangan Dokter</th>
-                <th>Tanda Tangan Fisioterapis</th>
+                <th rowspan="2">Program</th>
+                <th rowspan="2">Tanggal</th>
+                <th id="ttd" colspan="3">Tanda Tangan</th>
+            </tr>
+            <tr>
+                <th>Pasien</th>
+                <th>Dokter</th>
+                <th>Fisioterapis</th>
             </tr>
         </thead>
         <tbody>
